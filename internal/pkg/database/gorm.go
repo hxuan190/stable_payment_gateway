459fa2ae187dfea83d982e21df6eeb57c329b1bf
hxuan190 <hxuan190@gmail.com>
2025-11-27 21:53:44 +0700

    refactor: Remove SQLite driver and update GORM initialization for PostgreSQL

--- internal/pkg/database/gorm.go
+++ internal/pkg/database/gorm.go
@@ -1,15 +1,17 @@
 package database
 
 import (
+	"fmt"
 	"time"
 
-	"gorm.io/driver/sqlite"
+	"github.com/hxuan190/stable_payment_gateway/internal/config"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-func InitGORM(dsn string, maxOpenConns, maxIdleConns int) (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{
+func InitGORM(cfg *config.DatabaseConfig, maxOpenConns, maxIdleConns int) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
 	if err != nil {
